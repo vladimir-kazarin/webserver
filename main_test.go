bb980f41a7f9dfca5c3e0e9f950e5d326fcd2d10
vladimir-kazarin <vladimir_kazarin@yahoo.com>
2018-02-28 19:50:44 -0500

    adding unit tests for main.go

---
+++ main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
+
+func TestEventNextHandler(t *testing.T) {
+    request, _ := http.NewRequest("GET", "/linux", nil)
+    response := httptest.NewRecorder()
+    handler(response, request)
+    expected := "Hi there, I love linu!"
+    if response.Body.String() != expected {
+        t.Fatalf("Got: %s\nExpected: %s",
+            response.Body.String(), expected)
+    }
+}
