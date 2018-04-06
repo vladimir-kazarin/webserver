f1019f8330edb229e00dbb109c7cba50c59d1993
vladimir-kazarin <vladimir_kazarin@yahoo.com>
2018-04-06 00:13:10 -0400

    adding gcp instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,17 @@ terraform {
 provider "google" {
   region = "us-central1"
 }
+
+resource "google_compute_instance_template" "default" {
+  name = "tf-server"
+  disk {
+    image = "cos-stable"
+  }
+}
+
+resource "google_compute_instance_group_manager" "default" {
+  base_instance_name = "app"
+  instance_template  = "${google_compute_instance_template.tf-server.self_link}"
+  target_size = 2
+
+}
