47504781c48c84da7d7864de8cda2b49c793b61d
vladimir-kazarin <vladimir_kazarin@yahoo.com>
2018-04-18 18:58:25 -0400

    new version 2.0

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Now running version 2.0")
 }
 
 func main() {
