0f10dc8c02ecfb8d6af7a2f30096084d14f044fd
vladimir-kazarin <vladimir_kazarin@yahoo.com>
2018-04-18 19:11:16 -0400

    fixing test

--- main_test.go
+++ main_test.go
@@ -10,7 +10,7 @@ func TestEventNextHandler(t *testing.T) {
     request, _ := http.NewRequest("GET", "/linux", nil)
     response := httptest.NewRecorder()
     handler(response, request)
-    expected := "Hi there, I love linux!"
+    expected := "Now running version 2.0"
     if response.Body.String() != expected {
         t.Fatalf("Got: %s\nExpected: %s",
             response.Body.String(), expected)
