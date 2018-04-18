580e7b63c3326f86551c27c0728d94fe810a1a9e
vladimir-kazarin <vladimir_kazarin@yahoo.com>
2018-04-18 19:19:40 -0400

    adding staging instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,31 @@ resource "google_compute_instance_template" "tf-server" {
       stdin: false
       tty: false
     restartPolicy: Always
-  EOF
+EOF
+  }
+}
+
+resource "google_compute_instance_template" "tf-server" {
+  name = "tf-server2"
+  project = "comp698-vak1003"
+  disk {
+    source_image = "cos-cloud/cos-stable"
+  }
+  machine_type = "n1-standard-1"
+  network_interface {
+    network = "default"
+  }
+
+  metadata {
+      gce-container-declaration = <<EOF
+  spec:
+    containers:
+    - image: 'gcr.io/comp698-vak1003/github-vladimir-kazarin-webserver:1982b19d53ecbc9848bb809fd3ef019720975493'
+      name: service-container
+      stdin: false
+      tty: false
+    restartPolicy: Always
+EOF
   }
 }
 
@@ -40,7 +64,15 @@ resource "google_compute_instance_group_manager" "default" {
   base_instance_name = "app"
   instance_template  = "${google_compute_instance_template.tf-server.self_link}"
   target_size = 2
+}
 
+resource "google_compute_instance_group_manager" "default" {
+  name = "tf-manager2"
+  project = "comp698-vak1003"
+  zone = "us-central1-f"
+  base_instance_name = "app"
+  instance_template  = "${google_compute_instance_template.tf-server2.self_link}"
+  target_size = 1
 }
 
 resource "google_storage_bucket" "image-store" {
